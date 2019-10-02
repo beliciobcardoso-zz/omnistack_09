f7dfeac81f35d1657b3f60b2182d088ccc37ee31
Belicio Batista Cardoso <44432451+belloinfo@users.noreply.github.com>
2019-10-01 22:49:31 -0300

    backup 02

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://teste:323327@omnistack09-fqlcx.mongodb.net/semana09?retryWrites=true&w=majority',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 
 app.get('/', (req, res)=>{
     return res.json({message: "Falaaaaa Dev!!!!!"});
 });
 
-app.listen(3333); //escuta a porte 3333
+app.listen(3333); //escuta a porta 3333
---
+++ texto.md
@@ -0,0 +1,10 @@
+instalar o Yarn
+instalar dentro do backend
+1- express (yarn add express)
+2- nodemon (yarn add nodemon -D)
+3- mongoose (yarn add mongoose)
+
+executar
+yarn dev
+
+programa Insomnia
